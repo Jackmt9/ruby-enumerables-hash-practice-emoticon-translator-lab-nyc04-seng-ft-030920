94bf3a9d1c2be54cb1fe5381ba285a547b7fc409
Jack T <jackmt9@gmail.com>
2020-02-24 23:11:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  load_library(file_path)["get_emoticon"][emoticon]
+  load_library(file_path)["get_emoticon"][emoticon]? 
   # binding.pry
   
 end
